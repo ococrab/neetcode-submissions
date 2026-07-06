124f0a2656f1958f84eaf5bd20bb783348aea8e7
ococrab <126769888+ococrab@users.noreply.github.com>
2026-07-06 12:47:19 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,36 @@
+#include <stack>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class MinStack {
+private:
+    stack<int> stk;
+    stack<int> minStk;
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        if(minStk.empty()){
+            minStk.push(val);
+        }
+        else{
+            minStk.push(min(val, minStk.top()));
+        }
+    }
+    
+    void pop() {
+        stk.pop();
+        minStk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStk.top();
+    }
+};
